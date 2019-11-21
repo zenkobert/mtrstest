75cee9bfa3e2f00f954dde1644733db8a1310d88
Albert Zenko <zenkobert@gmail.com>
2019-11-21 15:35:53 +0700

    create DTO for user object

---
+++ src/main/java/com/zezenk/mtrstest/dto/UserDTO.java
@@ -0,0 +1,16 @@
+package com.zezenk.mtrstest.dto;
+
+import com.zezenk.mtrstest.model.Gender;
+
+public class UserDTO {
+
+    private Long id;
+    private String mobileNumber;
+    private String firstName;
+    private String lastName;
+    private Gender gender;
+    private int date;
+    private int month;
+    private int year;
+    private String email;
+}
--- src/main/java/com/zezenk/mtrstest/model/TbUser.java
+++ src/main/java/com/zezenk/mtrstest/model/TbUser.java
@@ -10,19 +10,26 @@ import java.time.LocalDate;
 @Entity
 @Setter
 @Getter
+@Table(name = "tb_user")
 public class TbUser {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "mobile_number")
     private String mobileNumber;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 
     @Enumerated(value = EnumType.STRING)
     private Gender gender;
 
+    @Column(name = "date_of_birth")
     private LocalDate dateOfBirth;
 
     @Email
